360e8a9e21dd1c01062d87d53bd7ece1162968fc
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-10-16 02:37:22 +0900

    qm.rb routine to get prime implicants (in solve())

--- qmalgo/qm.rb
+++ qmalgo/qm.rb
@@ -1,3 +1,14 @@
+module QMInternal
+	def bit_count(bits,mask)
+		bits&=mask
+		cnt=0
+		for i in 0...bits.size*8
+			cnt+=1 if (bits&(1<<i))!=0
+		end
+		return cnt
+	end
+end
+
 class QM
 	@n=0
 	@stat=[]
@@ -14,8 +25,10 @@ class QM
 			@stat[e[0]]=e[1]
 		end
 	end
+	include QMInternal
+	extend QMInternal
 	def solve()
-		rem=[]
+		rem=[] #rem=[[v1,m1], [v2,m2],...], v1:bitmask for true, ~v2:bitmask for don't care
 		on_pat=[]
 		mt=[] #Žå€
 		for i in 0...@stat.size()
@@ -31,7 +44,26 @@ class QM
 		print "rem="
 		p rem
 		while(rem.size()>0)
-			break
+			nxtbuf=[] #next rem
+			paired=Array.new(rem.size(),false)
+			for x in 0...rem.size()-1
+				for i in x+1...rem.size()
+					next unless rem[x][1]==rem[i][1]
+					mask=rem[x][1]
+					bc=bit_count(rem[x][0]^rem[i][0], mask)
+					if(bc==1)
+						paired[x]=paired[i]=true
+						new_pair=[rem[x][0]&rem[i][0],mask^rem[x][0]^rem[i][0]]
+						nxtbuf<<new_pair if (nxtbuf.index(new_pair)).nil?
+						puts "nxtbuf="+nxtbuf.inspect #debug
+					end
+				end
+			end
+			for i in 0...rem.size()
+				mt+=[rem[i]] if !paired[i]
+			end
+			puts "mt="+mt.inspect
+			rem=nxtbuf
 		end
 	end
 end
