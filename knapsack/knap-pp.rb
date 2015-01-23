0e36ba5098d35215165fdcc25eeab35ab2394611
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-01-23 20:35:39 +0900

    Fix bug in knap-pp

--- knapsack/knap-pp.rb
+++ knapsack/knap-pp.rb
@@ -9,18 +9,18 @@ def knapsack_dp(dat, bagsize)
     total_w += v
     inf += c
   end
-  dp = Array.new(n+1).map{|v| Array.new(total_w, 0)}
+  dp = Array.new(n + 1).map{|v| Array.new(total_w + 1, 0)}
   dp[0][0] = 0
   for j in 1..total_w
     dp[0][j] = inf # inf represents infinity
   end
   for i in 1..n
     for j in 0..total_w
-      dp[i][j] = dp[i-1][j]
-      w = dat[i-1][1]
-      s = dat[i-1][0]
+      dp[i][j] = dp[i - 1][j]
+      w = dat[i - 1][1]
+      s = dat[i - 1][0]
       if w <= j
-        dp[i][j] = [dp[i][j], s + dp[i-1][j-w]].min
+        dp[i][j] = [dp[i][j], s + dp[i - 1][j - w]].min
       else
         dp[i][j] = [dp[i][j], s].min
       end
@@ -36,10 +36,10 @@ def knapsack_dp(dat, bagsize)
   i = n
   rem = q
   while i >= 0
-    w = dat[i-1][1]
-    s = dat[i-1][0]
-    if dp[i][rem] - s == dp[i-1][rem-w]
-      set << (i-1)
+    w = dat[i - 1][1]
+    s = dat[i - 1][0]
+    if dp[i][rem] - s == dp[i - 1][rem - w]
+      set << (i - 1)
       rem -= w
     end
     i -= 1
@@ -52,7 +52,7 @@ end
 def knapsack_fptas(dat, bagsize, k)
   n = dat.size()
   max_w = dat.map{|v| v[1]}.max
-  scale = max_w.to_f / (k + 1) / n
+  scale = [1.0, max_w.to_f / (k + 1) / n].max
   dat_new = dat.map{|v, w| [v, (w / scale).to_i]}
   sub = knapsack_dp(dat_new, bagsize)
   [sub[1].map{|v| dat[v][1]}.reduce(:+), sub[1]]
