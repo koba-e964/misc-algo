2520445e9448c5caf58dfe85c47f7088028d658d
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-07-15 22:21:30 +0900

    Add integral_lll_reduce to lll.cpp

--- linear/lll.cpp
+++ linear/lll.cpp
@@ -65,7 +65,7 @@ lll_reduce(Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &basis) {
   cb[0] = bc.col(0).squaredNorm();
   typedef Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> MS;
   MS mu(n, n);
-  MS h(MatrixXd::Identity(n, n));
+  MS h(MS::Identity(n, n));
   // Step 2
   do {
     if (k > kmax) {
@@ -99,9 +99,106 @@ lll_reduce(Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &basis) {
   return h;
 }
 
+template<typename Scalar, int Cols, int Options, int MaxCols>
+void integral_red(int k,
+		  int l,
+		  Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> &lambda,
+		  vector<Scalar> &d,
+		  Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> &h) {
+  if (abs(lambda(k, l) * 2) <= d[l + 1]) {
+    return;
+  }
+  Scalar q = (2 * lambda(k, l) + d[l + 1]) / (2 * d[l + 1]); // nearest integer to lambda(k, l) / d[l + 1]
+  h.col(k) -= q * h.col(l);
+  lambda(k, l) -= q * d[l + 1];
+  for (int i = 0; i < l; ++i) {
+    lambda(k, i) -= q * lambda(l, i);
+  }
+}
+
+
+template<typename Scalar, int Cols, int Options, int MaxCols>
+void integral_swap(int k,
+		   Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> &lambda,
+		   vector<Scalar> &d,
+		   int kmax,
+		   Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> &h) {
+  int n = h.cols();
+  for (int i = 0; i < n; i++) {
+    swap(h(i, k), h(i, k - 1));
+  }
+  for (int j = 0; j < k - 1; ++j) {
+    swap(lambda(k, j), lambda(k - 1, j));
+  }
+  Scalar lam = lambda(k, k - 1);
+  Scalar B = (d[k - 1] * d[k + 1] + lam * lam) / d[k];
+  for (int i = k + 1; i <= kmax; ++i) {
+    Scalar t = lambda(i, k);
+    lambda(i, k) = (d[k + 1] * lambda(i, k - 1) - lam * t) / d[k];
+    lambda(i, k - 1) = t + lam * lambda(i, k) / B;
+  }
+  d[k] = B;
+}
+
+template<typename Scalar, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols>
+integral_lll_reduce(const Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &basis) {
+  int k = 1, kmax = 0;
+  int n = basis.cols(); // b[0] ... b[n - 1]
+  int m = basis.rows(); // dim b[i]
+  vector<Scalar> d(n + 1); // d[i] corresponds to d_i in the book
+  d[0] = 1;
+  d[1] = basis.col(0).squaredNorm();
+  typedef Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> MS;
+  MS lambda(n, n);
+  MS h(MS::Identity(n, n));
+  // Step 2
+  do {
+    if (k > kmax) {
+      kmax = k;
+      for (int j = 0; j <= k; ++j) {
+	Scalar u = basis.col(k).dot(basis.col(j));
+	for (int i = 0; i < j; ++i) {
+	  u = d[i + 1] * u - lambda(k, i) * lambda(j, i);
+	  u /= d[i];
+	}
+	if (j < k) {
+	  lambda(k, j) = u;
+	} else {
+	  d[k + 1] = u;
+	}
+      }
+      if (d[k + 1] == 0) {
+	cerr << "basis did not form a basis" << endl;
+	abort();
+      }
+    }
+    //Step 3
+    while (1) {
+      integral_red(k, k - 1, lambda, d, h);
+      if (4 * d[k + 1] * d[k - 1] < 3 * d[k] * d[k] - lambda(k, k - 1) * lambda(k, k - 1)) {
+	integral_swap(k, lambda, d, kmax, h);
+	k = max(1, k - 1);
+	continue;
+      }
+      break;
+    }
+    for (int l = k - 2; l >= 0; --l) {
+      integral_red(k, l, lambda, d, h);
+    }
+    ++k;
+  } while (k < n);
+  return h;
+}
+
+
 int main(void) {
   MatrixXd basis(3, 3);
   basis << 1, -1, 3,
+  1, 0, 5,
+  1, 2, 6;
+  MatrixXi basis_int(3, 3);
+  basis_int << 1, -1, 3,
   1, 0, 5,
   1, 2, 6;
   MatrixXd h = lll_reduce(basis);
@@ -109,4 +206,6 @@ int main(void) {
     cout << "basis[" << i << "]=" << basis.col(i) << endl;
   }
   cout << "H = " << h << endl;
+  MatrixXi h2 = integral_lll_reduce(basis_int);
+  cout << "H2 = " << h2 << endl;
 }
