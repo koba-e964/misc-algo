38bfc4db4a76468160b8407a3ec664c31ee66bb7
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2015-08-27 21:42:35 +0900

    [incomplete] Add dependent_lll_reduce

--- integral/lll.h
+++ integral/lll.h
@@ -190,3 +190,108 @@ integral_lll_reduce(const Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> 
   } while (k < n);
   return h;
 }
+
+template<typename Scalar, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+  void dependent_swap(int k, Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &basis,
+		      Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> &mu,
+		      vector<Scalar> &cb,
+		      int kmax,
+		      Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> &h,
+		      Scalar eps) {
+  for (int i = 0; i < mu.cols(); ++i) {
+    swap(basis(i, k), basis(i, k - 1));
+    swap(h(i, k), h(i, k - 1));
+  }
+  for (int j = 0; j < k - 1; ++j) {
+    swap(mu(k, j), mu(k - 1, j));
+  }
+  Scalar m = mu(k, k - 1);
+  Scalar b = cb[k] + m * m * cb[k - 1];
+  if (b < eps) {
+    swap(cb[k], cb[k - 1]);
+    for (int i = k + 1; i <= kmax; ++i) {
+      swap(mu(i, k), mu(i, k - 1));
+    }
+  } else if (cb[k] < eps && max(m, -m) > eps) {
+    cb[k - 1] = b;
+    mu(k, k - 1) = 1 / m;
+    for (int i = k + 1; i <= kmax; ++i) {
+      mu(i, k - 1) /= m;
+    }
+  } else {
+    Scalar t = cb[k - 1] / b;
+    mu(k, k - 1) = m * t;
+    cb[k] *= t;
+    cb[k - 1] = b;
+    for (int i = k + 1; i <= kmax; ++i) {
+      t = mu(i, k);
+      mu(i, k) = mu(i, k - 1) - m * t;
+      mu(i, k - 1) = t + mu(k, k - 1) * mu(i, k);
+    }
+  }
+}
+
+template<typename Scalar, int Rows, int Cols, int Options, int MaxRows, int MaxCols>
+  Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols>
+  dependent_lll_reduce(Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> &basis, int &p, Scalar eps) {
+  int k = 1, kmax = 0;
+  int n = basis.cols();
+  int m = basis.rows(); // dim b[i]
+  Matrix<Scalar, Rows, Cols, Options, MaxRows, MaxCols> bc(m, n); // b*
+  bc.col(0) = basis.col(0);
+  vector<Scalar> cb(n); // B
+  cb[0] = bc.col(0).squaredNorm();
+  typedef Matrix<Scalar, Cols, Cols, Options, MaxCols, MaxCols> MS;
+  MS mu(n, n);
+  MS h(MS::Identity(n, n));
+  // Step 2
+  do {
+    if (1) { // TODO debug code
+      cout << "** dump **" << endl;
+      cout << "k = " << k << ", kmax = " << kmax << endl;
+      cout << "H = " << h << endl;
+      cout << "basis = " << basis << endl;
+      cout << "b* = " << bc << endl;
+      cout << "mu = " << mu << endl;
+      cout << "B[] = {";
+      for (int i = 0; i < n; ++i) {
+	cout << cb[i] << ", ";
+      }
+      cout << "}" << endl;
+    }
+    if (k > kmax) {
+      kmax = k;
+      bc.col(k) = basis.col(k);
+      for (int j = 0; j < k; ++j) {
+	if (cb[j] >= eps) {
+	  mu(k, j) = basis.col(k).dot(bc.col(j)) / cb[j];
+	} else {
+	  mu(k,j) = 0;
+	}
+	bc.col(k) -= mu(k, j) * bc.col(j);
+      }
+      cout << "bc.col(" << k << ") = " << bc.col(k) << endl;
+      cb[k] = bc.col(k).squaredNorm();
+    }
+    //Step 3
+    while (1) {
+      red(k, k - 1, mu, basis, h);
+      if (cb[k] < (0.75 - pow(mu(k, k - 1), 2.0)) * cb[k - 1]) {
+	dependent_swap(k, basis, mu, cb, kmax, h, eps);
+	k = max(1, k - 1);
+	continue;
+      }
+      break;
+    }
+    for (int l = k - 2; l >= 0; --l) {
+      red(k, l, mu, basis, h);
+    }
+    ++k;
+  } while (k < n);
+  int r = 0;
+  for (int i = 0; i < n; i++) {
+    r += basis.col(i).squaredNorm() < eps ? 1 : 0;
+  }
+  p = n - r; // output p
+  return h;
+}
--- integral/lll_main.cpp
+++ integral/lll_main.cpp
@@ -1,14 +1,34 @@
 #include "./lll.h"
 
+void test_dependent(void) {
+  cout << "** test_dependent **" << endl;
+  MatrixXd basis(3, 3);
+  basis << 1, 2, 3,
+    1, 1, 2,
+    3, 0, 3;
+  /*  basis << 1,2,0,
+    1,1,0,
+    3,0,0;
+  */
+  int p;
+  MatrixXd h = dependent_lll_reduce(basis, p, 1e-9);
+  for (int i = 0; i < 3; ++i) {
+    cout << "basis[" << i << "]=" << basis.col(i) << endl;
+  }
+  cout << "p = " << p << endl;
+  cout << "H = " << h << endl;
+}
+
+
 int main(void) {
   MatrixXd basis(3, 3);
   basis << 1, -1, 3,
-  1, 0, 5,
-  1, 2, 6;
+    1, 0, 5,
+    1, 2, 6;
   MatrixXi basis_int(3, 3);
   basis_int << 1, -1, 3,
-  1, 0, 5,
-  1, 2, 6;
+    1, 0, 5,
+    1, 2, 6;
   MatrixXd h = lll_reduce(basis);
   for (int i = 0; i < 3; ++i) {
     cout << "basis[" << i << "]=" << basis.col(i) << endl;
@@ -16,4 +36,5 @@ int main(void) {
   cout << "H = " << h << endl;
   MatrixXi h2 = integral_lll_reduce(basis_int);
   cout << "H2 = " << h2 << endl;
+  test_dependent();
 }
