94c3b70c6e6d363dec2cb64da85e863af364c8ed
koba-e964 <koba_max@kobas-Air.pohe>
2016-10-04 23:36:39 +0900

    Fix lem.rb

--- simulate/lemniscate/lem.rb
+++ simulate/lemniscate/lem.rb
@@ -1,19 +1,28 @@
-def test()
-	delta=1e-3
-	n=3000
+# Calculates lemniscate constant
+def calc_lemniscate_constant(delta)
+	n=(3.0/delta).to_i
 	cur=0.0
-	ary=[]
+        x = 0.0
 	for i in 0...n
-		if i%100==0
-			p i
-		end
 		if(cur>=1.0)
 			break
 		end
-		x=i.to_f*delta
-		ary+=[[x,cur]]
-		dy=delta*((1-cur**4)**0.5)
-		cur+=dy
+		x = i.to_f * delta
+		dy = delta*((1-cur**4)**0.5)
+		cur += dy
 	end
-	ary
+	x
+end
+
+def test
+  step = [1.0, 0.5, 0.2]
+  for i in 0..6
+    for s in step
+      delta = s * 0.1 ** i
+      start_time = Time.now
+      lem_const = calc_lemniscate_constant(delta)
+      end_time = Time.now
+      puts "delta = #{delta}, lemConst = #{lem_const}, time = #{end_time - start_time}s"
+    end
+  end
 end
